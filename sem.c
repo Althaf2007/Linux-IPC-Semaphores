f71a3505de738263b9ab235a527f782b0170ef09
althaf <althaf@localhost.localdomain>
2026-09-08 08:40:47 +0200

    Initial commit

--- sem.c
+++ sem.c
@@ -1,1 +1,23 @@
-// C program that implements a producer-consumer system with two processes using Semaphores.
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+
+int main() {
+    sem_t s;
+    sem_init(&s, 1, 1);
+
+    if (fork() == 0) {
+        sem_wait(&s);
+        printf("Producer: Producing\n");
+        sem_post(&s);
+    } else {
+        wait(NULL);
+        sem_wait(&s);
+        printf("Consumer: Consuming\n");
+        sem_post(&s);
+    }
+
+    sem_destroy(&s);
+    return 0;
+}
